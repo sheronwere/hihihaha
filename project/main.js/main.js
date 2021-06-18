6472d30e7ae61a02aaf2e7beb26c4001bbb5e46d
sheronwere <86115025+sheronwere@users.noreply.github.com>
2021-06-18 17:00:44 +0300

    Add files via upload

---
+++ project/main.js/main.js
@@ -0,0 +1,6 @@
+const Year = 2021;
+let userYear = prompt("Введите ваш год рождения");
+// alert(`Ваш возраст ${2020 - YearOfBirth}`);
+alert(`Ваш возраст ${Year - userYear}`);
+let name = prompt("Введите ваше имя");
+alert(`Ваш возраст ${name}`);
